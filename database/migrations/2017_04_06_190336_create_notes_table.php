a7b39473dacb40ab1cef690f24ea324d4eae45ae
Miguel Huerta <miguelhuerta@internetbrands.com>
2017-04-06 12:10:25 -0700

    Created notes tables migration. Adjusted users table migration.

---
+++ database/migrations/2017_04_06_190336_create_notes_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // notes table
+        Schema::create('notes', function(Blueprint $table){
+
+            // user_id
+            $table->integer('user_id')->primary();
+
+            // title
+            $table->string('title');
+
+            // note
+            $table->text('note');
+
+            // timestamps
+            $table->timestamps();
+    });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // drop table
+        Schema::drop('notes');
+    }
+}
